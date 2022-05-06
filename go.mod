e5cac4946740aad8544c3f417d36e4b1dea0c4d1
Саргсян Юра Араикович <skullkon@gmail.com>
2022-05-06 16:37:55 +0700

    generate random UAs and parse them

--- go.mod
+++ go.mod
@@ -5,11 +5,20 @@ go 1.17
 require github.com/ClickHouse/clickhouse-go/v2 v2.0.14
 
 require (
+	github.com/EDDYCJY/fake-useragent v0.2.0 // indirect
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/corpix/uarand v0.1.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/mailru/go-clickhouse/v2 v2.0.0 // indirect
+	github.com/mileusna/useragent v1.0.2 // indirect
+	github.com/mssola/user_agent v0.5.3 // indirect
 	github.com/paulmach/orb v0.5.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/ua-parser/uap-go v0.0.0-20211112212520-00c877edfe0f // indirect
 	go.opentelemetry.io/otel v1.7.0 // indirect
 	go.opentelemetry.io/otel/trace v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,63 +1,138 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/ua-parser/uap-go/uaparser"
 	"log"
+	"math/rand"
 	"time"
 
-	"github.com/ClickHouse/clickhouse-go/v2"
+	browser "github.com/EDDYCJY/fake-useragent"
 )
 
+type Info struct {
+	Id             int32     `ch:"id"`
+	Ip             string    `ch:"ip"`
+	TypeOfDevice   string    `ch:"type"`
+	Os             string    `ch:"os"`
+	OsVersion      string    `ch:"osVersion"`
+	Browser        string    `ch:"browser"`
+	BrowserVersion string    `ch:"browserVersion"`
+	Brand          string    `ch:"Brand"`
+	Model          string    `ch:"model"`
+	Resolution     string    `ch:"resolution"`
+	Time           time.Time `ch:"time"`
+}
+
+type Browser struct {
+	Name    string
+	Version string
+}
+
 func main() {
-	conn, err := clickhouse.Open(&clickhouse.Options{
-		Addr: []string{"127.0.0.1:9000"},
-		Auth: clickhouse.Auth{
-			Database: "information",
-			Username: "default",
-			Password: "",
-		},
-		//Debug:           true,
-		DialTimeout:     time.Second,
-		MaxOpenConns:    10,
-		MaxIdleConns:    5,
-		ConnMaxLifetime: time.Hour,
-		Compression: &clickhouse.Compression{
-			Method: clickhouse.CompressionLZ4,
-		},
-	})
+	// conn, err := clickhouse.Open(&clickhouse.Options{
+	// 	Addr: []string{"127.0.0.1:9000"},
+	// 	Auth: clickhouse.Auth{
+	// 		Database: "information",
+	// 		Username: "default",
+	// 		Password: "",
+	// 	},
+	// 	//Debug:           true,
+	// 	DialTimeout:     time.Second,
+	// 	MaxOpenConns:    10,
+	// 	MaxIdleConns:    5,
+	// 	ConnMaxLifetime: time.Hour,
+	// 	Compression: &clickhouse.Compression{
+	// 		Method: clickhouse.CompressionLZ4,
+	// 	},
+	// })
+	// if err != nil {
+	// 	return
+	// }
+	// ctx := clickhouse.Context(context.Background(), clickhouse.WithSettings(clickhouse.Settings{
+	// 	"max_block_size": 10,
+	// }), clickhouse.WithProgress(func(p *clickhouse.Progress) {
+	// 	fmt.Println("progress: ", p)
+	// }))
+	// if err := conn.Ping(ctx); err != nil {
+	// 	if exception, ok := err.(*clickhouse.Exception); ok {
+	// 		fmt.Printf("Catch exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+	// 	}
+	// 	return
+	// }
+
+	// var result []Info
+
+	// if err := conn.Select(ctx, &result, "SELECT id, ip, type FROM info"); err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+
+	// batch, err := conn.PrepareBatch(ctx, "INSERT INTO info")
+	// if err != nil {
+	// 	return
+	// }
+
+	// for i := 0; i < 500_000; i++ {
+	// 	err := batch.Append(
+	// 		uint8(42),
+	// 		"ClickHouse", "Inc",
+	// 		uuid.New(),
+	// 		map[string]uint8{"key": 1},             // Map(String, UInt8)
+	// 		[]string{"Q", "W", "E", "R", "T", "Y"}, // Array(String)
+	// 		[]interface{}{ // Tuple(String, UInt8, Array(Map(String, String)))
+	// 			"String Value", uint8(5), []map[string]string{
+	// 				map[string]string{"key": "value"},
+	// 				map[string]string{"key": "value"},
+	// 				map[string]string{"key": "value"},
+	// 			},
+	// 		},
+	// 		time.Now(),
+	// 	)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// }
+	parser, err := uaparser.New("./regexes.yaml")
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
-	ctx := clickhouse.Context(context.Background(), clickhouse.WithSettings(clickhouse.Settings{
-		"max_block_size": 10,
-	}), clickhouse.WithProgress(func(p *clickhouse.Progress) {
-		fmt.Println("progress: ", p)
-	}))
-	if err := conn.Ping(ctx); err != nil {
-		if exception, ok := err.(*clickhouse.Exception); ok {
-			fmt.Printf("Catch exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+	for i := 0; i < 100; i++ {
+		t := browser.Random()
+		ua := parser.Parse(t)
+		agent := &Info{
+			Id:             GenID(100),
+			Ip:             GenIP(),
+			Os:             ua.Os.Family,
+			OsVersion:      ua.Os.Major,
+			Browser:        ua.UserAgent.Family,
+			BrowserVersion: ua.UserAgent.Major + "." + ua.UserAgent.Minor,
+			Brand:          ua.Device.Brand,
+			Model:          ua.Device.Model,
+			Resolution:     GenResolution(),
+			Time:           time.Time{},
 		}
-		return
+		b, err := json.Marshal(agent)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(b))
 	}
+}
 
-	var result []struct {
-		Id             int32     `ch:"id"`
-		Ip             string    `ch:"ip"`
-		TypeOfDevice   string    `ch:"type"`
-		Os             string    `ch:"os"`
-		OsVersion      string    `ch:"osVersion"`
-		Browser        string    `ch:"browser"`
-		BrowserVersion string    `ch:"browserVersion"`
-		Brand          string    `ch:"brand"`
-		Model          string    `ch:"model"`
-		Resolution     string    `ch:"resolution"`
-		Time           time.Time `ch:"time"`
-	}
+func GenID(max int) int32 {
+	rand.Seed(time.Now().UnixNano())
+	return int32(rand.Intn(max))
+}
 
-	if err := conn.Select(ctx, &result, "SELECT id, ip, type FROM info"); err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Println(result)
+func GenIP() string {
+	ip := fmt.Sprintf("%d:%d:%d:%d", GenID(255), GenID(255), GenID(255), GenID(255))
+	return ip
+}
+
+func GenResolution() string {
+	resolution := fmt.Sprintf("%dx%d", GenID(4000), GenID(4000)/2)
+	return resolution
 }
