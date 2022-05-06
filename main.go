9ca0dda54024eac3214859b3c993a98c95e72302
Саргсян Юра Араикович <skullkon@gmail.com>
2022-05-06 17:25:26 +0700

    fill db random data

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
+	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ua-parser/uap-go/uaparser"
 	"log"
 	"math/rand"
@@ -31,79 +32,56 @@ type Browser struct {
 }
 
 func main() {
-	// conn, err := clickhouse.Open(&clickhouse.Options{
-	// 	Addr: []string{"127.0.0.1:9000"},
-	// 	Auth: clickhouse.Auth{
-	// 		Database: "information",
-	// 		Username: "default",
-	// 		Password: "",
-	// 	},
-	// 	//Debug:           true,
-	// 	DialTimeout:     time.Second,
-	// 	MaxOpenConns:    10,
-	// 	MaxIdleConns:    5,
-	// 	ConnMaxLifetime: time.Hour,
-	// 	Compression: &clickhouse.Compression{
-	// 		Method: clickhouse.CompressionLZ4,
-	// 	},
-	// })
-	// if err != nil {
-	// 	return
-	// }
-	// ctx := clickhouse.Context(context.Background(), clickhouse.WithSettings(clickhouse.Settings{
-	// 	"max_block_size": 10,
-	// }), clickhouse.WithProgress(func(p *clickhouse.Progress) {
-	// 	fmt.Println("progress: ", p)
-	// }))
-	// if err := conn.Ping(ctx); err != nil {
-	// 	if exception, ok := err.(*clickhouse.Exception); ok {
-	// 		fmt.Printf("Catch exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
-	// 	}
-	// 	return
-	// }
+	conn, err := clickhouse.Open(&clickhouse.Options{
+		Addr: []string{"127.0.0.1:9000"},
+		Auth: clickhouse.Auth{
+			Database: "information",
+			Username: "default",
+			Password: "",
+		},
+		//Debug:           true,
+		DialTimeout:     time.Second,
+		MaxOpenConns:    10,
+		MaxIdleConns:    5,
+		ConnMaxLifetime: time.Hour,
+		Compression: &clickhouse.Compression{
+			Method: clickhouse.CompressionLZ4,
+		},
+	})
+	if err != nil {
+		return
+	}
+	ctx := clickhouse.Context(context.Background(), clickhouse.WithSettings(clickhouse.Settings{
+		"max_block_size": 10,
+	}), clickhouse.WithProgress(func(p *clickhouse.Progress) {
+		fmt.Println("progress: ", p)
+	}))
+	if err := conn.Ping(ctx); err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			fmt.Printf("Catch exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		}
+		return
+	}
 
-	// var result []Info
+	var result []Info
 
-	// if err := conn.Select(ctx, &result, "SELECT id, ip, type FROM info"); err != nil {
-	// 	log.Println(err)
-	// 	return
-	// }
+	if err := conn.Select(ctx, &result, "SELECT id, ip, type FROM info"); err != nil {
+		log.Println(err)
+		return
+	}
 
-	// batch, err := conn.PrepareBatch(ctx, "INSERT INTO info")
-	// if err != nil {
-	// 	return
-	// }
-
-	// for i := 0; i < 500_000; i++ {
-	// 	err := batch.Append(
-	// 		uint8(42),
-	// 		"ClickHouse", "Inc",
-	// 		uuid.New(),
-	// 		map[string]uint8{"key": 1},             // Map(String, UInt8)
-	// 		[]string{"Q", "W", "E", "R", "T", "Y"}, // Array(String)
-	// 		[]interface{}{ // Tuple(String, UInt8, Array(Map(String, String)))
-	// 			"String Value", uint8(5), []map[string]string{
-	// 				map[string]string{"key": "value"},
-	// 				map[string]string{"key": "value"},
-	// 				map[string]string{"key": "value"},
-	// 			},
-	// 		},
-	// 		time.Now(),
-	// 	)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
 	parser, err := uaparser.New("./regexes.yaml")
 	if err != nil {
 		log.Fatal(err)
 	}
-	for i := 0; i < 100; i++ {
+	var uaList []Info
+	for i := 0; i < 10000; i++ {
 		t := browser.Random()
 		ua := parser.Parse(t)
-		agent := &Info{
+		agent := Info{
 			Id:             GenID(100),
 			Ip:             GenIP(),
+			TypeOfDevice:   ua.Device.Family,
 			Os:             ua.Os.Family,
 			OsVersion:      ua.Os.Major,
 			Browser:        ua.UserAgent.Family,
@@ -111,14 +89,41 @@ func main() {
 			Brand:          ua.Device.Brand,
 			Model:          ua.Device.Model,
 			Resolution:     GenResolution(),
-			Time:           time.Time{},
+			Time:           time.Now(),
 		}
-		b, err := json.Marshal(agent)
+		uaList = append(uaList, agent)
+
+	}
+
+	batch, err := conn.PrepareBatch(ctx, "INSERT INTO info")
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < 10000; i++ {
+		err := batch.Append(
+			uaList[i].Id,
+			uaList[i].Ip,
+			uaList[i].TypeOfDevice,
+			uaList[i].Os,
+			uaList[i].OsVersion,
+			uaList[i].Browser,
+			uaList[i].BrowserVersion,
+			uaList[i].Brand,
+			uaList[i].Model,
+			uaList[i].Resolution,
+			uaList[i].Time,
+		)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Print(err)
 			return
 		}
-		fmt.Println(string(b))
+	}
+	err = batch.Send()
+	if err != nil {
+		fmt.Print(err)
+
+		return
 	}
 }
 
@@ -133,6 +138,21 @@ func GenIP() string {
 }
 
 func GenResolution() string {
-	resolution := fmt.Sprintf("%dx%d", GenID(4000), GenID(4000)/2)
-	return resolution
+	resolutions := []string{
+		"320×240", "352×240", "352×288",
+		"400×240", "480×576", "640×240",
+		"320×480", "640×360", "640×480",
+		"800×480", "800×600", "848×480",
+		"960×540", "1024×600", "1024×768",
+		"1152×864", "1200×600", "1280×720",
+		"1280×768", "1280×1024", "1440×900",
+		"1400×1050", "1536×960", "1536×1024",
+		"1600×900", "1600×1024", "1600×1200",
+		"1680×1050", "1920×1080", "1920×1200",
+		"2048×1080", "2048×1152", "2048×1536",
+		"2560×1440", "2560×1600", "2560×2048",
+		"3072×1620", "3200×1800", "3200×2048",
+		"3200×2400", "3440×1440", "3840×2400",
+	}
+	return resolutions[GenID(len(resolutions))]
 }
