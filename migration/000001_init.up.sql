8864fd64368fe0f117d9fecf45cc91e5e8e13206
Саргсян Юра Араикович <skullkon@gmail.com>
2022-05-09 00:41:40 +0700

    refactor main

---
+++ migration/000001_init.up.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS information;
+CREATE TABLE IF NOT EXISTS info
+(
+    id             UUID,
+    ip             String,
+    type           String,
+    os             String,
+    osVersion      String,
+    browser        String,
+    browserVersion String,
+    brand          String,
+    model          String,
+    resolution     String,
+    time           DateTime
+) ENGINE = MergeTree()
+      PARTITION BY toYYYYMM(time)
+      ORDER BY (id, browser, brand, os);
+
